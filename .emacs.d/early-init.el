145cab66daa733e46a649086ebbc63efbe803aaf
CToID <funk443@yandex.com>
2022-07-02 03:29:11 +0800

    Add a function to toggle transparency of emacs

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -12,7 +12,9 @@
   (make-directory "~/.emacs.d/undo_tree"))
 
 (add-to-list 'default-frame-alist '(font . "NovaMono-12"))
-(set-frame-parameter (selected-frame) 'alpha '(97 . 100))
-(add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+
+(set-frame-parameter (selected-frame) 'alpha '(90 . 75))
+(add-to-list 'default-frame-alist '(alpha . (90 . 75)))
+
 (defun display-startup-echo-area-message ()
   (message "ad astra per aspera"))
