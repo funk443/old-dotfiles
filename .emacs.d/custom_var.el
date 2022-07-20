650b4594bdc1e8e708ddfdd7248f43e75359795c
CToID <funk443@yandex.com>
2022-07-20 12:15:21 +0800

    Minor fix

--- .emacs.d/custom_var.el
+++ .emacs.d/custom_var.el
@@ -40,9 +40,3 @@
  '(tool-bar-mode nil)
  '(window-divider-mode t)
  '(x-select-enable-clipboard-manager nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
