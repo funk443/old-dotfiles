0d86681f486cde089a1363ef6e6a6f761ddf888f
ctoid <funk443@yandex.com>
2022-08-11 10:39:38 +0800

    Minor Change

--- .emacs.d/custom_var.el
+++ .emacs.d/custom_var.el
@@ -12,7 +12,6 @@
  '(css-indent-offset 2)
  '(current-language-environment "UTF-8")
  '(custom-safe-themes '(default))
- '(default-input-method "chinese-array30")
  '(display-line-numbers-type 'relative)
  '(emms-volume-change-function 'emms-player-mpv-raise-volume)
  '(fringe-mode 0 nil (fringe))
