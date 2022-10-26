69fea3dc3d25b10efc20c9c249f101092a3fabee
ctoid <funk443@yandex.com>
2022-10-26 12:51:22 +0800

    Use pipewire, and change to emacs keybindings

--- qtile_startup_once.sh
+++ qtile_startup_once.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
+pipewire &
+pipewire-pulse &
+pipewire-media-session &
 picom -CGb --experimental-backends
 ibus-daemon -drxR
 xfce4-clipman &
@@ -8,4 +12,3 @@ xinput set-button-map 8 1 2 3 4 5 6 7 8 2 10 11 12
 numlockx
 discord &
 emacs --daemon 
-pulseaudio &
