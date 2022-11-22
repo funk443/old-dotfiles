cec9d0e8831e2fae44f33ddd1bfb2d3102a1ae34
ctoid <funk443@yandex.com>
2022-11-22 21:26:08 +0800

    Remove unneeded picom flag

--- qtile_startup_once.sh
+++ qtile_startup_once.sh
@@ -4,10 +4,11 @@ xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920
 pipewire &
 pipewire-pulse &
 wireplumber &
-picom -CGb --experimental-backends
+picom -CGb
 ibus-daemon -drxR
 xfce4-clipman &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 xinput set-button-map 8 1 2 3 4 5 6 7 8 2 10 11 12
 numlockx
 discord &
+emacs --daemon &
