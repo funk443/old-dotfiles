4d33d2784e14e54ea7f1ac2c36074fcf76c6aedc
ctoid <funk443@yandex.com>
2023-02-13 17:36:58 +0800

    Remove build-deps after building is completed

--- misc/make-emacs.sh
+++ misc/make-emacs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo xbps-install gtk+3-devel libmagick-devel webkit2gtk-devel libgccjit-devel libXpm-devel gnutls-devel jansson-devel tree-sitter-devel giflib-devel ncurses-devel libwebp-devel librsvg-devel
+sudo xbps-install gtk+3-devel libmagick-devel webkit2gtk-devel libgccjit-devel libXpm-devel gnutls-devel jansson-devel tree-sitter-devel giflib-devel ncurses-devel libwebp-devel librsvg-devel tree-sitter
 
 ./autogen.sh
 ./configure --with-x-toolkit=yes --with-imagemagick --with-json --with-tree-sitter --with-xwidgets --with-native-compilation=yes --prefix=/usr
@@ -8,3 +8,5 @@ make -j$(nproc)
 
 sudo rm /bin/emacs
 sudo ln -rs src/emacs /bin/emacs
+
+sudo xbps-remove -Ro gtk+3-devel libmagick-devel webkit2gtk-devel libgccjit-devel libXpm-devel gnutls-devel jansson-devel tree-sitter-devel giflib-devel ncurses-devel libwebp-devel librsvg-devel
