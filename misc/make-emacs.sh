170666efc15a64440dac54ba72fd113fc574fee9
ctoid <funk443@yandex.com>
2023-02-13 17:16:29 +0800

    A script to build emacs

---
+++ misc/make-emacs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo xbps-install gtk+3-devel libmagick-devel webkit2gtk-devel libgccjit-devel libXpm-devel gnutls-devel jansson-devel tree-sitter-devel giflib-devel ncurses-devel libwebp-devel librsvg-devel
+
+./autogen.sh
+./configure --with-x-toolkit=yes --with-imagemagick --with-json --with-tree-sitter --with-xwidgets --with-native-compilation=yes --prefix=/usr
+make -j$(nproc)
+
+sudo rm /bin/emacs
+sudo ln -rs src/emacs /bin/emacs
