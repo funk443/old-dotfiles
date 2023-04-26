aeac259f67f3825619be3d68a6f25ee0d03f219b
ctoid <funk443@yandex.com>
2023-04-26 21:44:37 +0800

    Some changes in styles, and add WM_CLASS to modeline and msg window

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -9,16 +9,32 @@
 (setf *screen-mode-line-format* (list "%g | %d")
       *time-modeline-string* "%a. %d %b %y %T"
       *mode-line-timeout* 2
-      *window-format* "<[%n%s]%t>"
+      *window-format* "<[%n%s%m]%t>"
+      *window-info-format* "%wx%h %n %m (%t)"
       *mouse-focus-policy* :click
       *group-format* "<[%n%s]%t>"
       *message-window-padding* 3
       *message-window-y-padding* 3
+      *maxsize-border-width* 2
+      *window-border-style* 
+      *transient-border-width* 2
+      *normal-border-width* 2
       swm-gaps:*head-gaps-size* 0
       swm-gaps:*inner-gaps-size* 5
       swm-gaps:*outer-gaps-size* 5)
 
+(let ((unfocus-color "#4e4e4e")
+      (focus-color "#478061"))
+  (set-focus-color focus-color)
+  (set-float-focus-color focus-color)
+  (set-unfocus-color unfocus-color)
+  (set-float-unfocus-color unfocus-color))
+
+(toggle-gaps-on)
 (set-font "-ibm-courier-bold-r-*-*-17-120-100-100-*-*-*-*")
+
+(xlib:set-wm-class (screen-message-window (current-screen))
+                   "stumpwm-message" "stumpwm-message")
 
 (grename "firefox")
 (gnewbg "discord")
@@ -32,9 +48,12 @@
 (add-hook *quit-hook* (lambda ()
                         (run-shell-command
                          "emacsclient -e '(save-buffers-kill-emacs)'")))
-
 (add-hook *focus-group-hook* (lambda (new-group old-group)
                                (message "~a" new-group)))
+(add-hook *new-mode-line-hook* (lambda (mode-line)
+                                 (xlib:set-wm-class (mode-line-window mode-line)
+                                                    "stumpwm-modeline"
+                                                    "stumpwm-modeline")))
 
 (defcommand current-cpu-usage () ()
   (message "~a" (run-shell-command "mpstat" t)))
@@ -59,7 +78,6 @@
                             (cons (kbd "s") "hsplit")
                             (cons (kbd "S") "vsplit")
                             (cons (kbd "m") "mode-line")
-                            (cons (kbd "C-k") "remove-split")
                             (cons (kbd "a") "my-echo-date"))))
   (dolist (key keys-to-define)
     (define-key *root-map* (car key) (cdr key))))
