8e026dd2c5e4a217989fbbfa99347d57a8a64569
ctoid <funk443@yandex.com>
2023-04-27 19:53:40 +0800

    Clear stuff up

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,7 +1,6 @@
 (setq custom-file (locate-user-emacs-file "custom-var.el")
       gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.8)
-      ;; initial-frame-alist '((width . 160)))
 
 (load custom-file 'noerror 'nomessage)
 (unless (file-directory-p "~/.emacs.d/autosave")
@@ -10,10 +9,6 @@
   (make-directory "~/.emacs.d/backup"))
 (unless (file-directory-p "~/.emacs.d/undo_tree")
   (make-directory "~/.emacs.d/undo_tree"))
-
-;; (add-to-list 'default-frame-alist '(font . "NovaMono-12"))
-;; (add-to-list 'default-frame-alist '(font . "ShareTechMono-12"))
-;; (add-to-list 'default-frame-alist '(font . "IBMPlexMono-12"))
 
 (set-frame-parameter (selected-frame) 'alpha-background 85)
 (add-to-list 'default-frame-alist '(alpha-background . 85))
