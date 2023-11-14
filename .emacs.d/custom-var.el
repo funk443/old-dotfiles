c7c60fb9bfeee2f9671402ff8b7ba5f6fc5d3e9e
CToID <funk443@yandex.com>
2023-11-14 14:48:29 +0800

    Minor changes on mode line formats

--- .emacs.d/custom-var.el
+++ .emacs.d/custom-var.el
@@ -141,6 +141,7 @@
  '(line-spacing nil)
  '(major-mode-remap-alist '((perl-mode . cperl-mode)))
  '(menu-bar-mode nil)
+ '(mode-line-compact t)
  '(mode-line-position-column-line-format '(" (%l %c)"))
  '(org-agenda-files '("~/Documents/agenda.org"))
  '(org-capture-templates
@@ -174,10 +175,10 @@
  '(org-todo-keywords '((sequence "TODO(t)" "DONE(d)")))
  '(org-use-sub-superscripts '{})
  '(package-selected-packages
-   '(consult eat marginalia emms magit markdown-mode racket-mode dashboard all-the-icons-ibuffer unfill sly magit-section htmlize treesit-auto vterm-toggle ladger-mode orderless vertico org-roam screenshot doom-themes
-             #("eshell-prompt-extras" 0 20
-               (escaped t))
-             "eshell-prompt-extras" all-the-icons-dired use-package arduino-mode xclip yaml-mode visual-fill-column visual-fill-column-mode visual-line-mode highlight-indent-guides org-present org-present-mode toc-org sudo-edit neotree which-key ##))
+   '(all-the-icons consult eat marginalia emms magit markdown-mode racket-mode dashboard all-the-icons-ibuffer unfill sly magit-section htmlize treesit-auto vterm-toggle ladger-mode orderless vertico org-roam screenshot doom-themes
+                   #("eshell-prompt-extras" 0 20
+                     (escaped t))
+                   "eshell-prompt-extras" all-the-icons-dired use-package arduino-mode xclip yaml-mode visual-fill-column visual-fill-column-mode visual-line-mode highlight-indent-guides org-present org-present-mode toc-org sudo-edit neotree which-key ##))
  '(read-buffer-completion-ignore-case t)
  '(read-file-name-completion-ignore-case t)
  '(require-final-newline t)
