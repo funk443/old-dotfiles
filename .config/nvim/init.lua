67303f7bd962806eed9243a482e1898a84f016d5
CToID <funk443@yandex.com>
2024-04-27 18:42:41 +0800

    fuck tabs.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {
-	"junegunn/vim-easy-align"
+  "junegunn/vim-easy-align"
 }
 require("lazy").setup(plugins, opts)
 
 vim.api.nvim_create_autocmd(
-	{"BufWritePre"},
-	{command = "%s/\\s\\+$//e"}
+  {"BufWritePre"},
+  {command = "%s/\\s\\+$//e"}
 )
 
 vim.opt.cursorline = true
 vim.opt.tw = 80
 vim.opt.cc = '+0'
 vim.opt.tabstop = 2
+vim.opt.expandtab = true
 vim.opt.shiftwidth = 0
 vim.opt.termguicolors = true
 vim.opt.background = "light"
